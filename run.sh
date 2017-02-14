07c8421a614feeff451996baa0b1686e4bb0299a
nishil <nishyl@gmail.com>
2017-02-14 10:52:46 +0530

    run.sh update

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker rm uod
 docker run -d --name "uod" -t -i -v $PWD:/usr/src/app -p 3000:3000 --net dockernet charsi/uod
 docker exec -d uod gulp start
 docker ps
-echo "NOTICE: All done. UOD container is porbally running on port 3000"
+echo "NOTICE: All done! UOD container is porbally running on port 3000"
