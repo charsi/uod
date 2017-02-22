eb80120d158b6c9f5c706b63ef6819dea4bc7bc8
nishil <nishyl@gmail.com>
2017-02-22 16:54:02 +0530

    added indian comma system

--- js-src/client.js
+++ js-src/client.js
@@ -760,6 +760,14 @@ function getCurrencySymbol(currencyCode){
 }
 
 function commaSeparateNumber(val){
+	if (country==='IN'){
+		var x=val.toString();
+		var lastThree = x.substring(x.length-3);
+		var otherNumbers = x.substring(0,x.length-3);
+		if(otherNumbers != '')
+    	lastThree = ',' + lastThree;
+		return otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+	} else
     while (/(\d+)(\d{3})/.test(val.toString())){
       val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
     }
