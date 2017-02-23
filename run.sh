a8eb403dcbe2c0cfc5dccc318e6195b25c2f9284
nishil <nishyl@gmail.com>
2017-02-23 09:41:30 +0530

    add separator after currency abbreviation

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-docker stop uod
-docker rm uod
+docker rm -f uod
 docker run -d --name "uod" -t -i -v $PWD:/usr/src/app -p 3000:3000 --net dockernet charsi/uod
-docker exec -it uod gulp start
+docker exec -d uod gulp start
 docker ps
 echo "NOTICE: All done! UOD container started. Try http://localhost:3000/"
 echo "NOTICE: use \"docker exec -it uod /bin/bash\" to log in to the container "
