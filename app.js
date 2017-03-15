7e5e865cb36b4467f14e5a8dde4e8336ac0b4fce
nishil <nishyl@gmail.com>
2017-03-15 18:51:15 +0530

    new logo, colour sc heme

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
+var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 
 var routes = require('./routes/index');
 
@@ -16,7 +17,7 @@ app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
