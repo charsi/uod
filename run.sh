5cb2041bf565dc75a2d331813c410faa10688f61
Nishil <nishilfoo@gmail.com>
2018-11-11 14:53:44 -0800

    no npm start reqd

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ echo "NOTICE: \"docker attach uod\" to attach"
 
 # 127.0.0.1 forces the app to be accesible from localhost only
 # remove it to make the app publically accesible without using a reverse proxy (like nginx)
-docker run --name "uod" -ti --rm -v $PWD/public:/src/public -p 127.0.0.1:8000:8000 charsi/uod npm start
+docker run --name "uod" -ti --rm -v $PWD/public:/src/public -p 127.0.0.1:8000:8000 charsi/uod
 
 # being called from Dockerfile --
 # docker exec uod npm run gulp start-live
